25840d5ca72c75947614f7f46c70713696a68aee
Anshul Sharma <anshulsharma427@gmail.com>
2018-01-22 10:39:14 -0500

    added comments

--- fibonacciSeries.java
+++ fibonacciSeries.java
@@ -1,12 +1,12 @@
 public class Fibonacci{
   public static void main (String[] args){
-    int[] s=new int[40];                         //memory allocation
+    int[] s=new int[40];                         //memory allocation.
     s[0]=0;
     s[1]=1;
     System.out.print(s[0]+" "+s[1]);
-    for(int i=2;i<40;i++){                     // not fron zero because first two terms already printed
+    for(int i=2;i<40;i++){                     // not fron zero because first two terms already printed.
       s[i]=s[i-1]+s[i-2];
-    System.out.print(" "+s[i]);               //space for proper indentation
+    System.out.print(" "+s[i]);               //space for proper indentation.
     }
   }
 }
