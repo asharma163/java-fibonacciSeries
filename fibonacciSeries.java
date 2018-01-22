f62c6981d1301d7ff08ae659a0b5eb059fb81d52
Anshul Sharma <anshulsharma427@gmail.com>
2018-01-22 10:34:19 -0500

    Create fibonacciSeries.java

---
+++ fibonacciSeries.java
@@ -0,0 +1,14 @@
+public class Fibonacci{
+  public static void main (String[] args){
+    int[] s=new int[40];
+    s[0]=0;
+    s[1]=1;
+    System.out.print(s[0]+" "+s[1]);
+    for(int i=2;i<40;i++){
+      s[i]=s[i-1]+s[i-2];
+    System.out.print(" "+s[i]);
+    }
+  }
+}
+    
+      
